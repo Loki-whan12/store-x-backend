aadaba1cc77256496865b4e2284a844fbb2ea3ff
Maalug Jonathan <jmaalug11@gmail.com>
2024-09-10 08:23:49 +0000

    updated sellers model

--- app/models/seller.py
+++ app/models/seller.py
@@ -1,15 +1,28 @@
 from app import db
 from datetime import datetime
+from app.models.user import User
+
 
 class Seller(db.Model):
     __tablename__ = 'sellers'
-    seller_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    seller_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), unique=True, nullable=False)
-    company_name = db.Column(db.String(255))
-    business_license_number = db.Column(db.String(50))
+    username = db.Column(db.String(50), db.ForeignKey('users.username'), unique=True, nullable=False)
+    seller_name = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    
-    user = db.relationship('User', backref='sellers')
 
-    
+    # Establishing a relationship with the User model
+    user = db.relationship('User', backref='seller', uselist=False)
+
+    def to_dict(self):
+        return {
+            'seller_id': self.seller_id,
+            'user_id': self.user_id,
+            'username': self.username,
+            'seller_name': self.seller_name,
+            'password': self.password,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+    }
